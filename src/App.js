1c092c3cdc3ac58950658f21b5fdc147b47b0f89
qtotuan <quynh.totuan@gmail.com>
2017-08-15 17:59:08 -0400

    Add User Sign Up Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect, Link, Switch} from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/users/login'
 import { connect } from 'react-redux'
 import FetchHabits from './actions/fetchHabits'
 import { bindActionCreators } from 'redux'
+import SignUpForm from './components/users/signup'
 
 class App extends Component {
 
@@ -17,10 +19,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to Habit Tracker!</h2>
-        </div>
-        <Login />
+        <div className="App-header"><h2>Welcome to Habit Tracker!</h2></div>
+        <Router>
+          <div>
+            <Route exact path='/' component={Login} />
+            <Route exact path='/signup' component={SignUpForm} />
+          </div>
+        </Router>
       </div>
     );
   }
---
+++ src/components/users/signup.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import { Container, Button, Divider, Form } from 'semantic-ui-react'
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import SetUser from '../../actions/setUser'
+
+class SignUpForm extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      redirect: false
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/api/v1/signup', {
+      method: 'POST',
+      body: JSON.stringify(this.state),
+      headers: {
+        'content-type': 'application/json',
+        'accept': 'application/json',
+        // 'Authorization': localStorage.getItem('jwt')
+      }
+    })
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      localStorage.setItem('email', json.user.email)
+      this.props.setUser(json.user)
+      this.setState({ redirect: true })
+  })
+}
+
+  handleChange = (event) => {
+    let key = `${event.target.name}`
+    let value = `${event.target.value}`
+    this.setState({
+      [key]: value
+    })
+  }
+
+  render() {
+    return (
+      <Container>
+        {this.state.redirect ? <Redirect to="/habits"/> : null }
+        <h1>Sign Up</h1>
+        <Form size='large' id='signup-form' onSubmit={this.handleSubmit}>
+          <Form.Field name="first_name" label='First name' control='input' placeholder='First name' onChange={this.handleChange}/>
+          <Form.Field name="last_name" label='Last name' control='input' placeholder='Last name'  onChange={this.handleChange}/>
+          <Form.Field name="email" label='Email' control='input' placeholder='Email address'  onChange={this.handleChange}/>
+          <Button type='submit'>Submit</Button>
+          <Divider hidden />
+        </Form>
+      </Container>
+    )
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({
+    setUser: SetUser
+  }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(SignUpForm)
